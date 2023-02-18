c0a9af5865266e5e3350ecf3ef154f04be774b75
bigdmike <piggy5272@gmail.com>
2023-02-18 12:00:00 +0800

    Commit: Update styling and structure of MainFooterNav component

--- src/components/MainFooterNav.vue
+++ src/components/MainFooterNav.vue
@@ -1,45 +1,48 @@
 <template>
-  <nav class="fixed bottom-0 left-0 right-0 z-20 block md:hidden" v-if="show">
-    <ol class="flex items-stretch w-full border-t border-zinc-300">
-      <li class="w-1/4 text-center border-r border-zinc-300">
+  <nav
+    class="fixed z-20 block w-11/12 max-w-lg transform -translate-x-1/2 rounded-lg bottom-5 left-1/2 md:hidden bg-basic_black"
+    v-if="show"
+  >
+    <ol class="flex items-stretch w-full">
+      <li class="w-1/4 text-center border-r border-white border-opacity-10">
         <button
           @click="$router.push('/')"
-          class="flex items-center justify-center w-full py-3 text-white bg-primary sm:py-4"
+          class="flex items-center justify-center w-full h-full py-3 text-white sm:py-4"
         >
-          <HomeIcon class="w-5 h-5 sm:w-6 sm:h-6" />
+          <span class="text-xl text-white icon-home"></span>
         </button>
       </li>
-      <li class="w-1/4 text-center border-r border-zinc-300">
+      <li class="w-1/4 text-center border-r border-white border-opacity-10">
         <button
           @click="$emit('open-menu')"
-          class="flex items-center justify-center w-full py-3 bg-white text-primary sm:py-4"
+          class="flex items-center justify-center w-full h-full py-3 text-white sm:py-4"
         >
-          <ListIcon class="w-5 h-5 sm:w-6 sm:h-6" />
+          <span class="text-xl text-white icon-menu"></span>
         </button>
       </li>
-      <li class="w-1/4 text-center border-r border-zinc-300">
+      <li class="w-1/4 text-center border-r border-white border-opacity-10">
         <button
           @click="OpenShopCart"
-          class="flex items-center justify-center w-full py-3 bg-white text-primary sm:py-4"
+          class="flex items-center justify-center w-full h-full pb-[10px] pt-[14px] text-white sm:py-4"
         >
           <div class="relative">
             <span
-              class="absolute top-0 right-0 flex items-center justify-center w-4 h-4 transform translate-x-2 -translate-y-2 rounded-full bg-primary"
+              class="absolute top-0 right-0 flex items-center justify-center w-4 h-4 transform translate-x-2 -translate-y-1 rounded-full bg-primary"
             >
               <p class="text-xs font-bold text-white transform scale-75">
                 {{ shopcart_count }}
               </p>
             </span>
-            <ShopCartIcon class="relative z-0 w-5 h-5 sm:w-6 sm:h-6" />
+            <span class="text-xl leading-none text-white icon-cart"></span>
           </div>
         </button>
       </li>
       <li class="w-1/4 text-center">
         <button
           @click="$router.push('/account/login')"
-          class="flex items-center justify-center w-full py-3 bg-white text-primary sm:py-4"
+          class="flex items-center justify-center w-full h-full py-3 text-white sm:py-4"
         >
-          <MemberIcon class="w-5 h-5 sm:w-6 sm:h-6" />
+          <span class="text-xl text-white icon-search"></span>
         </button>
       </li>
     </ol>
@@ -47,18 +50,9 @@
 </template>
 
 <script>
-import HomeIcon from '@/components/svg/HomeIcon.vue';
-import ListIcon from '@/components/svg/ListIcon.vue';
-import MemberIcon from '@/components/svg/MemberIcon.vue';
-import ShopCartIcon from '@/components/svg/ShopCartIcon.vue';
 export default {
   name: 'MainFooterNav',
-  components: {
-    HomeIcon,
-    ListIcon,
-    MemberIcon,
-    ShopCartIcon,
-  },
+  components: {},
   data() {
     return {
       show: true,
